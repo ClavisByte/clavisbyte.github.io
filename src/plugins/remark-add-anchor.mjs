609e7c3ed4dca597b79327ff13c288e99688bdbe
ClavisByte <clavisbyte@gmail.com>
2024-10-23 20:49:15 -0700

    Fresh clone, test deploy

---
+++ src/plugins/remark-add-anchor.mjs
@@ -0,0 +1,27 @@
+import { visit } from 'unist-util-visit';
+
+export function remarkAddAnchor() {
+    return function (tree) {
+        let headingCount = 0;
+        visit(tree, 'heading', (node) => {
+            headingCount++;
+            const headingId = `heading-${headingCount}`;
+
+            if (!node.data) {
+                node.data = {};
+            }
+            if (!node.data.hProperties) {
+                node.data.hProperties = {};
+            }
+
+            node.data.hProperties.id = headingId;
+
+            const anchorHtml = {
+                type: 'html',
+                value: `<a href="#${headingId}" class="anchor"><span class="anchor-icon" data-pagefind-ignore="">#</span></a>`,
+            };
+
+            node.children.push(anchorHtml);
+        });
+    };
+}
